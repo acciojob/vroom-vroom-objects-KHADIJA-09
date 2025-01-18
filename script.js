9a3467c4ee673d039e8b255ac18b0a8970543c13
KHADIJA-09 <132650876+KHADIJA-09@users.noreply.github.com>
2025-01-18 23:41:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-// Complete the js code
-function Car(make, model) {}
+}
+// Car Class
+class Car {
+    constructor(make, model) {
+        this.make = make;
+        this.model = model;
+    }
 
-function SportsCar(make, model, topSpeed) {}
+    getMakeModel() {
+        return `${this.make} ${this.model}`;
+    }
+}
+
+// SportsCar Class extending Car
+class SportsCar extends Car {
+    constructor(make, model, topSpeed) {
+        super(make, model);  // Call the parent class constructor
+        this.topSpeed = topSpeed;
+    }
+
+    getTopSpeed() {
+        return this.topSpeed;
+    }
+}
+
+// Testing
+// const car = new SportsCar("Ferrari", "Testarossa", 200);
+// console.log(car.getMakeModel());  // Output: Ferrari Testarossa
+// console.log(car.getTopSpeed());   // Output: 200
 
 // Do not change the code below
 window.Car = Car;
